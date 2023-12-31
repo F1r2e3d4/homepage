8003654cc1c9fc14370cad9ba5e8a7209046bc19
F1r2e3d4 <leo707679@gmail.com>
2023-12-30 22:40:21 -0800

    fixed the display and mobile navigation

--- main.js
+++ main.js
@@ -4,20 +4,13 @@ const darkregion = document.getElementById('dark');
 const closing = document.getElementById('icon-menu-close');
 
 function hiddenmenu(){
-    if (menu.style.display === 'none'){
         menu.style.display = 'block';
         darkregion.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-        darkregion.style.display = 'none';
-    }
 }
 function reveal(){
-    if(menu.style.display == 'block' &&   darkregion.style.display == 'block'){
         menu.style.display = 'none';
         darkregion.style.display = 'none';
     }
-}
 
 button.addEventListener('click', hiddenmenu);
 closing.addEventListener('click', reveal);
